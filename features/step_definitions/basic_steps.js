051361811f564d066364d4d52c864dd5a6e59260
Raoul DIFFOUO <diraulo@gmail.com>
2018-04-10 14:28:59 +0200

    we can now click on add contact button

--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -1,4 +1,4 @@
-const { After, Given, Then } = require('cucumber')
+const { After, Given, Then, When } = require('cucumber')
 
 After(async function() {
   return await this.closeHomePage()
@@ -10,4 +10,23 @@ Given('I visit the site', async function() {
 
 Then('I should see {string}', async function(content) {
   return await this.pageHasTextContent(content)
+})
+
+When('I click {string}', async function(string) {
+  return await this.clickOnAddContactBtn()
+})
+
+Then('I fill in {string} with {string}', function(string, string2) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should have {int} contact in my address book', function(int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should not see {string}', function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
 })
