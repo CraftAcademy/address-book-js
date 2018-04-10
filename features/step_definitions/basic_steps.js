925ff29cc3d2df2afb2156800fe6d8f2aa67ba6f
Raoul DIFFOUO <diraulo@gmail.com>
2018-04-10 13:43:05 +0200

    First step goes green

--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -1,16 +1,19 @@
-const { Given, Then } = require("cucumber");
+const { After, Given, Then } = require('cucumber')
 
-Given("I visit the site", function() {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending";
-});
+After(async function() {
+  return await this.closeHomePage()
+})
 
-Then("I should see {string}", function(string) {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending";
-});
+Given('I visit the site', async function() {
+  return await this.openHomePage()
+})
 
-Then("I should see {string}", function(string) {
+Then('I should see {string}', function(string) {
   // Write code here that turns the phrase above into concrete actions
-  return "pending";
-});
+  return 'pending'
+})
+
+Then('I should see {string}', function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
